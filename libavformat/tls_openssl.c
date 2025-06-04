03a7865c91b136aa2b248c754fbffa99c3154078
Andarwinux <Andarwinux@users.noreply.github.com>
2025-06-04 00:00:00 +0000

    avformat/tls_openssl: use winstore as the fallback cert provider
    SSL_CTX_set_default_verify_paths is a fallback path when user didn't specify a CA file, but on Windows it still won't point to the system certificate store, so replacing it with SSL_CTX_load_verify_store to load winstore ensures TLS verify is available by default

--- libavformat/tls_openssl.c
+++ libavformat/tls_openssl.c
@@ -703,7 +703,7 @@ static av_cold int openssl_init_ca_key_cert(URLContext *h)
         if (!SSL_CTX_load_verify_locations(c->ctx, s->ca_file, NULL))
             av_log(h, AV_LOG_ERROR, "SSL_CTX_load_verify_locations %s\n", openssl_get_error(c));
     } else {
-        if (!SSL_CTX_set_default_verify_paths(c->ctx)) {
+        if (!SSL_CTX_load_verify_store(c->ctx, "org.openssl.winstore://")) {
             // Only log the failure but do not error out, as this is not fatal
             av_log(h, AV_LOG_WARNING, "Failure setting default verify locations: %s\n",
                 openssl_get_error(c));
