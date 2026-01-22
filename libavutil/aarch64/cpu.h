689063f3e1f811347242d94bf473807fb7c9c42a
Andarwinux <Andarwinux@users.noreply.github.com>
2026-01-22 00:00:00 +0000

    add static dispatch for aarch64

--- libavutil/aarch64/cpu.h
+++ libavutil/aarch64/cpu.h
@@ -22,19 +22,64 @@
 #include "libavutil/cpu.h"
 #include "libavutil/cpu_internal.h"
 
-#define have_armv8(flags)   CPUEXT(flags, ARMV8)
-#define have_neon(flags)    CPUEXT(flags, NEON)
-#define have_vfp(flags)     CPUEXT(flags, VFP)
-#define have_arm_crc(flags) CPUEXT(flags, ARM_CRC)
+#define have_armv8(flags)   1
+#define have_neon(flags)    1
+#define have_vfp(flags)     1
+#define have_arm_crc(flags) 1
+
+#if defined(__ARM_FEATURE_AES)
+#define have_pmull(flags)   1
+#else
 #define have_pmull(flags)   CPUEXT(flags, PMULL)
+#endif
+
+#if defined(__ARM_FEATURE_SHA3)
+#define have_eor3(flags)    1
+#else
 #define have_eor3(flags)    CPUEXT(flags, EOR3)
+#endif
+
+#if defined(__ARM_FEATURE_DOTPROD)
+#define have_dotprod(flags) 1
+#else
 #define have_dotprod(flags) CPUEXT(flags, DOTPROD)
+#endif
+
+#if defined(__ARM_FEATURE_MATMUL_INT8)
+#define have_i8mm(flags)    1
+#else
 #define have_i8mm(flags)    CPUEXT(flags, I8MM)
+#endif
+
+#if defined(__ARM_FEATURE_SVE)
+#define have_sve(flags)     1
+#else
 #define have_sve(flags)     CPUEXT(flags, SVE)
+#endif
+
+#if defined(__ARM_FEATURE_SVE2)
+#define have_sve2(flags)    1
+#else
 #define have_sve2(flags)    CPUEXT(flags, SVE2)
-#define have_sme(flags)     CPUEXT(flags, SME)
-#define have_sme_i16i64(flags) CPUEXT(flags, SME_I16I64)
-#define have_sme2(flags)    CPUEXT(flags, SME2)
+#endif
+
+#if defined(__ARM_FEATURE_SME)
+#define have_sme(flags)     1
+#else
+#define have_sme(flags)     0
+#endif
+
+#if defined(__ARM_FEATURE_SME_I16I64)
+#define have_sme_i16i64(flags) 1
+#else
+#define have_sme_i16i64(flags) 0
+#endif
+
+#if defined(__ARM_FEATURE_SME2)
+#define have_sme2(flags)    1
+#else
+#define have_sme2(flags)    0
+#endif
 
 #if HAVE_SVE
 int ff_aarch64_sve_length(void);
